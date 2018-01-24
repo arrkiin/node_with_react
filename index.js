9a189854c5b87f62abf2dcc78b1dbb49e5f5b2cb
arrkiin <arrkiin@gmail.com>
2018-01-24 11:04:43 +0100

    Make the code es5 conform

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000, // 30 Days
         keys: [keys.cookieKey],
-    }),
+    })
 );
 app.use(passport.initialize());
 app.use(passport.session());
